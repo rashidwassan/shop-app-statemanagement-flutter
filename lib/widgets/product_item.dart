9d8745c357ae28a7acfd40523f4095ba3cdb0651
Rashid Wassan <rashidwassaan@gmail.com>
2021-08-20 19:02:43 +0500

    added project structure, themedata, products gridview

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem(
+      {required this.id, required this.title, required this.imageUrl, Key? key})
+      : super(key: key);
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: GridTile(
+        child: Image.network(imageUrl, fit: BoxFit.cover),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          title: Text(title, textAlign: TextAlign.center),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
